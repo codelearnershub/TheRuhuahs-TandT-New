3f7a8a29f70a60072e6ce914b8aa1ec12b0a528e
oyeleyeoyediran <oyeleyeoyediran20@gmail.comgi>
2021-10-28 21:23:09 +0100

    Modified Packages

--- TheRuhuahs-TandTNew/Migrations/20211028155919_Mad migration.Designer.cs
+++ TheRuhuahs-TandTNew/Migrations/20211028155919_Mad migration.Designer.cs
@@ -2,15 +2,17 @@
 using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using TheRuhuahs_TandTNew.Context;
 
 namespace TheRuhuahs_TandTNew.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20211028155919_Mad migration")]
+    partial class Madmigration
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ TheRuhuahs-TandTNew/Migrations/20211028155919_Mad migration.cs
@@ -0,0 +1,112 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace TheRuhuahs_TandTNew.Migrations
+{
+    public partial class Madmigration : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                table: "Roles",
+                keyColumn: "Id",
+                keyValue: 1,
+                column: "CreatedAt",
+                value: new DateTime(2021, 10, 28, 16, 59, 19, 41, DateTimeKind.Local).AddTicks(9265));
+
+            migrationBuilder.UpdateData(
+                table: "Roles",
+                keyColumn: "Id",
+                keyValue: 2,
+                column: "CreatedAt",
+                value: new DateTime(2021, 10, 28, 16, 59, 19, 41, DateTimeKind.Local).AddTicks(9895));
+
+            migrationBuilder.UpdateData(
+                table: "Roles",
+                keyColumn: "Id",
+                keyValue: 3,
+                column: "CreatedAt",
+                value: new DateTime(2021, 10, 28, 16, 59, 19, 41, DateTimeKind.Local).AddTicks(9904));
+
+            migrationBuilder.UpdateData(
+                table: "UserRoles",
+                keyColumn: "Id",
+                keyValue: 1,
+                column: "CreatedAt",
+                value: new DateTime(2021, 10, 28, 16, 59, 19, 42, DateTimeKind.Local).AddTicks(1731));
+
+            migrationBuilder.UpdateData(
+                table: "UserRoles",
+                keyColumn: "Id",
+                keyValue: 2,
+                column: "CreatedAt",
+                value: new DateTime(2021, 10, 28, 16, 59, 19, 42, DateTimeKind.Local).AddTicks(2009));
+
+            migrationBuilder.UpdateData(
+                table: "Users",
+                keyColumn: "Id",
+                keyValue: 1,
+                column: "DateOfBirth",
+                value: new DateTime(2021, 10, 28, 16, 59, 19, 38, DateTimeKind.Local).AddTicks(580));
+
+            migrationBuilder.UpdateData(
+                table: "Users",
+                keyColumn: "Id",
+                keyValue: 2,
+                column: "DateOfBirth",
+                value: new DateTime(2021, 10, 28, 16, 59, 19, 41, DateTimeKind.Local).AddTicks(7765));
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                table: "Roles",
+                keyColumn: "Id",
+                keyValue: 1,
+                column: "CreatedAt",
+                value: new DateTime(2021, 10, 28, 3, 27, 4, 610, DateTimeKind.Local).AddTicks(2530));
+
+            migrationBuilder.UpdateData(
+                table: "Roles",
+                keyColumn: "Id",
+                keyValue: 2,
+                column: "CreatedAt",
+                value: new DateTime(2021, 10, 28, 3, 27, 4, 610, DateTimeKind.Local).AddTicks(3383));
+
+            migrationBuilder.UpdateData(
+                table: "Roles",
+                keyColumn: "Id",
+                keyValue: 3,
+                column: "CreatedAt",
+                value: new DateTime(2021, 10, 28, 3, 27, 4, 610, DateTimeKind.Local).AddTicks(3398));
+
+            migrationBuilder.UpdateData(
+                table: "UserRoles",
+                keyColumn: "Id",
+                keyValue: 1,
+                column: "CreatedAt",
+                value: new DateTime(2021, 10, 28, 3, 27, 4, 610, DateTimeKind.Local).AddTicks(5986));
+
+            migrationBuilder.UpdateData(
+                table: "UserRoles",
+                keyColumn: "Id",
+                keyValue: 2,
+                column: "CreatedAt",
+                value: new DateTime(2021, 10, 28, 3, 27, 4, 610, DateTimeKind.Local).AddTicks(6380));
+
+            migrationBuilder.UpdateData(
+                table: "Users",
+                keyColumn: "Id",
+                keyValue: 1,
+                column: "DateOfBirth",
+                value: new DateTime(2021, 10, 28, 3, 27, 4, 604, DateTimeKind.Local).AddTicks(9625));
+
+            migrationBuilder.UpdateData(
+                table: "Users",
+                keyColumn: "Id",
+                keyValue: 2,
+                column: "DateOfBirth",
+                value: new DateTime(2021, 10, 28, 3, 27, 4, 610, DateTimeKind.Local).AddTicks(452));
+        }
+    }
+}
